667cbb9d53691ad957c803e6745cb069f8879ad3
Seyeon <100107654+seyeon618@users.noreply.github.com>
2023-02-23 08:43:44 +0900

    feat: add password validation (#6)
    비밀번호에 숫자만 입력 가능하도록 정규식 추가
    
    Co-authored-by: Seyeon Jo <seyeon.jo@corelinesoft.com>

--- client/src/pages/create/index.tsx
+++ client/src/pages/create/index.tsx
@@ -1,19 +1,51 @@
 import Link from "next/link";
+import { useState } from "react";
 
 export default function Create() {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [password, setPassword] = useState("");
+  const [isPassword, setIsPassword] = useState(false);
+
+  const handleInputPw = (password: string) => {
+    // password regular expression
+    const pwRegEx = /^[0-9]+$/;
+
+    if (password.match(pwRegEx) === null) {
+      setIsPassword(false);
+    } else {
+      setIsPassword(true);
+    }
+  };
+
+  const handleNavigateToCake = (e: React.FormEvent<HTMLInputElement>) => {
+    if (!isPassword) {
+      e.preventDefault();
+      alert("비밀번호는 숫자로만 이루어져야 합니다.");
+    }
+  };
+
   return (
     <>
       <main>This is Create Page</main>
       <form>
         <input type="text" placeholder="To" />
-        <input type="text" placeholder="Password" />
+        <input
+          placeholder="Password"
+          type="password"
+          onChange={(e) => {
+            setPassword(e.target.value);
+            handleInputPw(e.target.value);
+          }}
+        />
         <input type="text" placeholder="Birthday" />
         <div>케이크 디자인</div>
-      </form>
 
-      <li>
-        <Link href="/cake">완료!</Link>
-      </li>
+        <li> cake design1 </li>
+
+        <Link href="/cake" onClick={() => handleNavigateToCake}>
+          <button>Next</button>
+        </Link>
+      </form>
     </>
   );
 }
