fa667a241e2d8ef4d4fa483c53aa781c7742fa72
Seo Jaeyong <wg7363@naver.com>
2023-02-26 16:52:24 +0900

    fix: congratulation main page (#32)

--- client/src/component/Page/CongratulationPage/index.tsx
+++ client/src/component/Page/CongratulationPage/index.tsx
@@ -9,6 +9,7 @@ import Cake1 from "@/asset/images/Cake_1.png";
 import Cake2 from "@/asset/images/Cake_2.png";
 import Cake3 from "@/asset/images/Cake_3.png";
 import Cake4 from "@/asset/images/Cake_4.png";
+import getCake from "@/apis/get/getCake";
 
 interface Props {
   cakeId: number;
@@ -19,6 +20,8 @@ const cakeImages = [Cake1, Cake2, Cake3, Cake4];
 function CongratulationPage({ cakeId }: Props) {
   const router = useRouter();
 
+  const { data } = getCake(cakeId);
+
   const goCongratulationPage = () => {
     router.push(`/cake/${cakeId}/congratulation`);
   };
@@ -26,9 +29,7 @@ function CongratulationPage({ cakeId }: Props) {
   return (
     <Container>
       <Image src={CongratulationText} width={319} height={54} alt="celebrate_text" />
-      <ImageWrap>
-        <Image src={cakeImages[cakeId - 1]} alt={`cake-image-${cakeId}`} width={355} height={355} />
-      </ImageWrap>
+      <ImageWrap>{data && <Image src={cakeImages[data.cake_design_id - 1]} alt={`cake-image-${cakeId}`} width={355} height={355} />}</ImageWrap>
       <Card></Card>
       <MakeButtonWrap>
         <Button label="Open Cake" Action={goCongratulationPage} />
--- client/src/pages/cake/[id]/index.tsx
+++ client/src/pages/cake/[id]/index.tsx
@@ -11,14 +11,6 @@ function Cake() {
   const { id } = router.query;
   const { data, isLoading } = getCake(Number(id));
 
-  // const data = {
-  //   id: 2,
-  //   receiver: "한승웅",
-  //   date_of_birth: "2023-02-13",
-  //   cake_design_id: 1,
-  //   link: null,
-  // };
-
   const isBeforeBirthday = new Date(data?.date_of_birth as string) > new Date();
 
   return <CommonBackground>{isBeforeBirthday ? <CelebratePage cakeId={data?.id} /> : <CongratulationPage cakeId={Number(id)} />}</CommonBackground>;
